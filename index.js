dbfc45e5323a077023f6e30c3c0be9dd1b61344c
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-11-10 12:16:51 +0600

    cors added and server site started

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
